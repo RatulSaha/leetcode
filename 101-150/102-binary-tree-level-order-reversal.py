07a4314fb7e4e6a9a042f4a3b85c70f9a61e4f4e
Ratul Saha <RatulSaha@users.noreply.github.com>
2017-02-04 11:02:47 +0530

    added number 102

---
+++ 101-150/102-binary-tree-level-order-reversal.py
@@ -0,0 +1,36 @@
+"""
+STATEMENT
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+CLARIFICATIONS
+- Do we print all 'None' values that a level may have? Yes.
+
+EXAMPLES
+[3,9,20,null,null,15,7] -> [[3],[9,20],[15,7]]
+
+COMMENTS
+- We remember the current level of nodes, the next level of nodes and the values of the current level of nodes.
+- Then, we update the level with the next level and keep going until the level is empty.
+"""
+
+def levelOrder(root):
+      """
+      :type root: TreeNode
+      :rtype: List[List[int]]
+      """
+      to_return = []
+      if not root:
+          return to_return
+      level = [root]
+
+      while level:
+          next_level, current_level_val = [], []
+          for node in level:
+              if node is not None:
+                  current_level_val.append(node.val)
+                  next_level.append(node.left)
+                  next_level.append(node.right)
+          level = next_level
+          if current_level_val:
+              to_return.append(current_level_val)
+      return to_return
